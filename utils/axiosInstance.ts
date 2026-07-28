834daeaa02d4c10d18a9fea400cf7218fbf662f4
Ian Calica <Iggc654@gmail.com>
2026-07-28 21:57:18 +0800

    Added token passing to axios request

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const DEVELOPMENT_URL = "http://192.168.1.64:8080/api/";
 const PRODUCTION_URL = "https://kalangka-756cdff24eb2.herokuapp.com/api/";
@@ -11,5 +12,29 @@ const client = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// Request interceptor to add auth token
+client.interceptors.request.use(
+  async (config) => {
+    try {
+      // Get token from AsyncStorage
+      const token = await AsyncStorage.getItem("token");
+
+      // If token exists, add it to headers
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+
+      return config;
+    } catch (error) {
+      console.error("Error in request interceptor:", error);
+      return config;
+    }
+  },
+  (error) => {
+    // Handle request error
+    return Promise.reject(error);
+  },
+);
 
 export default client;
